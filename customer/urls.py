92c13c13e021f4bbe5360a5475c3fae1e486a579
Wellington <wellrc96@gmail.com>
2023-03-13 19:45:20 -0300

    require login for other pages

--- customer/urls.py
+++ customer/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from .views import CustomerListView, CustomerCreateView, CustomerUpdateView, CustomerDeleteView
+from .views import (
+    CustomerListView,
+    CustomerCreateView, 
+    CustomerUpdateView, 
+    CustomerDeleteView
+)
+from django.contrib.auth.decorators import login_required
 
 app_name = "customer"
 
 # Rotas do customer
 urlpatterns = [
-    path("list/", CustomerListView.as_view(), name="customer-list"),
-    path("create/", CustomerCreateView.as_view(), name="customer-create"),
-    path("<int:id>/", CustomerUpdateView.as_view(), name="customer-update"),
-    path("<int:id>/delete/", CustomerDeleteView.as_view(), name="customer-delete")
+    path("list/", login_required(CustomerListView.as_view()), name="customer-list"),
+    path("create/", login_required(CustomerCreateView.as_view()), name="customer-create"),
+    path("<int:id>/", login_required(CustomerUpdateView.as_view()), name="customer-update"),
+    path("<int:id>/delete/", login_required(CustomerDeleteView.as_view()), name="customer-delete")
 ]
